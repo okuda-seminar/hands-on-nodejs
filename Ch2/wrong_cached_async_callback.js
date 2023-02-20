4e85ef0bbf6dcb6054f5dff7dbe18cd7a9762273
ryuji0123 <ryo1011.javelin@gmail.com>
2023-02-20 23:41:06 +0900

    Add wrong_cached_async_callback.js

---
+++ Ch2/wrong_cached_async_callback.js
@@ -0,0 +1,32 @@
+function parseJsonAsync(json, callback) {
+    setTimeout(() => {
+        try {
+            callback(JSON.parse(json))
+        } catch(err) {
+            console.log('catch')
+            callback(err)
+        }
+    }, 10)
+}
+
+const cache = {}
+function parseJsonAsyncWithCache(json, callback) {
+    const cached = cache[json]
+    if (cached) {
+        callback(cached.err, cached.result)
+        return
+    }
+    parseJsonAsync(json, (err, result) => {
+        cache[json] = {err, result}
+        callback(err, result)
+    })
+}
+
+parseJsonAsyncWithCache('{"message": "Hello", "to": "World"}', (err, result) => {
+    console.log('1', err, result)
+    parseJsonAsyncWithCache('{"message": "Hello", "to": "World"}', (err, result) => {
+        console.log('2', err, result)
+    })
+    console.log('2;')
+})
+console.log('1;')
