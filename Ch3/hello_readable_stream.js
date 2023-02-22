fb00c1c0c5f27da265ba544a750e71a9cebf6785
ryuji0123 <ryo1011.javelin@gmail.com>
2023-02-23 00:41:27 +0900

    Add hello_readable_stream.js

---
+++ Ch3/hello_readable_stream.js
@@ -0,0 +1,33 @@
+const stream = require('stream');
+
+class HelloReadableStream extends stream.Readable {
+  constructor(options) {
+    super(options)
+    this.languages = ['JavaScript', 'Python', 'Java', 'C#'];
+  }
+
+  _read(size) {
+    console.log('_read()');
+    let language;
+    while ((language = this.languages.shift())) {
+      if(!this.push(`Hello, ${language}\n`)) {
+        console.log('Stop reading');
+        return;
+      }
+    }
+    console.log('Finish reading');
+    this.push(null);
+  }
+}
+
+
+const readabledStream = new HelloReadableStream();
+readabledStream
+  .on('readable', () => {
+    console.log('readable');
+    let chunk;
+    while((chunk = readabledStream.read()) !== null) {
+      console.log(`chunk: ${chunk.toString()}`);
+    }
+  })
+  .on('end', () => console.log('end'));
