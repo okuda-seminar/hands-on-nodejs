dd621b30a1da546623b272fb2b860faabe456d69
ryuji0123 <ryo1011.javelin@gmail.com>
2023-02-28 23:34:55 +0900

    Add web_app.js

---
+++ Ch4/multi_thread_pool/web_app.js
@@ -0,0 +1,14 @@
+'use strict';
+const http = require('http');
+const cpuCount = require('os').cpus().length;
+const ThreadPool = require('../thread_pool');
+const threadPool = new ThreadPool(cpuCount, `${__dirname}/fibonacci.js`);
+
+http.createServer(async (req, res) => {
+  const n = Number(req.url.substring(1));
+  if (Number.isNaN(n)) {
+    return res.end();
+  }
+  const result = await threadPool.executeInThread(n);
+  res.end(result.toString());
+}).listen(3000);
