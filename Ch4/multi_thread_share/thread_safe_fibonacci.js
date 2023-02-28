4cceb501e1ca3a0ed8e6373875a1c61db5553572
ryuji0123 <ryo1011.javelin@gmail.com>
2023-03-01 00:26:49 +0900

    Add thread_safe_fibonacci.js

---
+++ Ch4/multi_thread_share/thread_safe_fibonacci.js
@@ -0,0 +1,8 @@
+'use strict';
+const fibonacci = require('../fibonacci');
+const { workerData: int32Array, parentPort } = require('worker_threads');
+
+parentPort.on('message', n => {
+  parentPort.postMessage(fibonacci(n));
+  Atomics.add(int32Array, 0, 1);
+})
